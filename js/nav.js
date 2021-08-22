a182930d718864c25f47b98d2d4f8e9b911b332d
HarishST <harishstprojects@gmail.com>
2021-08-22 10:06:21 +0530

    Code Initial Push

---
+++ js/nav.js
@@ -0,0 +1,57 @@
+$(document).ready(function()
+{
+	$('.MenuIcon').click(function()
+	{
+		$('#navigate').toggle().addClass('open');
+	});
+	$('#navigate > .menu-item-has-children').each(function()
+	{
+		$(this).prepend('<span class="touch"></span>');
+
+		$(this).find('> span').click(function() {
+			$(this).parent().find('> ul').toggle();
+		});
+	});
+	
+	$('#menu2').click(function()
+	{
+		$('#navigate2').toggle().addClass('open');
+	});
+	$('#navigate2 > .menu-item-has-children').each(function()
+	{
+		$(this).prepend('<span class="touch"></span>');
+
+		$(this).find('> span').click(function() {
+			$(this).parent().find('> ul').toggle();
+		});
+	});
+	
+	
+	$('.sub-menu .sub-menu').each(function()
+	{
+		$(this).parents('li').addClass('miniChild');
+	});
+	$width = $( document ).width();
+	if($width>800)
+	{
+		$('.sub-menu .menu-item-has-children a').hover(function()
+		{
+			$(this).closest('.item-with-ul').find('ul').toggle();
+
+		});	
+	 }
+	$('.sub-menu .menu-item-has-children').each(function()
+	{
+		$(this).prepend('<span class="plus"></span>');
+	});
+
+	$('.sub-menu .menu-item-has-children').each(function()
+	{
+		$(this).find('> span').click(function() {
+			$(this).parent().find('> ul').toggle();
+		});
+	});
+});
+
+
+
